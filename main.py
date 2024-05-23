edacb0f750eef8980523868b4a975dd1f6935c57
asss-whom <3059749821@qq.com>
2024-05-23 17:13:16 +0800

    Version 1.2.0

--- main.py
+++ main.py
@@ -1,49 +1,12 @@
-import atexit
 import argparse
-import functools
-import random
-import time
-
-from dataclasses import dataclass
-from typing import Any
+from pathlib import Path
 
 from rich.progress import Progress
 
-from utils import *
+from utils import Crawler, log
 
 
-@dataclass
-class FullInfo:
-    name: str
-    total: int
-    chpts: list[str]
-
-    @property
-    def is_finished(self) -> bool:
-        return len(self.chpts) == self.total
-
-    @property
-    def progress(self) -> int:
-        return len(self.chpts)
-
-
-@dataclass
-class RangeInfo:
-    name: str
-    lower_bound: int
-    upper_bound: int
-    chpts: list[str]
-
-    @property
-    def is_finished(self) -> bool:
-        return len(self.chpts) == self.upper_bound - self.lower_bound
-
-    @property
-    def progress(self) -> int:
-        return self.lower_bound + len(self.chpts)
-
-
-def create_parser():
+def main() -> None:
     parser = argparse.ArgumentParser(description="🕸️ 基于DrissionPage库的起点小说爬虫。")
     parser.add_argument(
         "-m",
@@ -69,97 +32,68 @@ def create_parser():
         default=None,
         help='范围下载的下界（仅当选择 "range" 模式时有效）',
     )
+    args = parser.parse_args()
 
-    return parser
+    if args.mode == "full":
+        full_download(args.url)
+    else:
+        if args.upper_bound is None or args.lower_bound is None:
+            parser.error("在范围模式下，必须同时提供 --upper-bound 和 --lower-bound")
+        range_donwload(args.url, args.lower_bound, args.upper_bound)
 
 
-def download_range_content(url: str, lower_bound: int, upper_bound: int) -> None:
+def save(name: str, content: str) -> None:
+    path = Path(f"{name}.txt")
+    path.write_text(content, "utf-8")
+
+
+def full_download(url: str) -> None:
+    crawler = Crawler()
+    log.info("🎉 DrissionPage初始化完毕")
+
+    index = crawler.get_index(url)
+    log.info(f"🎈 正在下载{index.name}，具有{len(index.chpts)}章节的小说")
+
+    chpts: list[str] = []
+    with Progress() as progress:
+        download = progress.add_task("🛻 下载中")
+        try:
+            for info in index.chpts:
+                chpt = crawler.get_chpt(info.url)
+                chpts.append(chpt)
+                progress.advance(download)
+        except Exception as e:
+            log.error(e)
+        finally:
+            content = "\n".join(chpts)
+            save(index.name, content)
+            log.info("✨ 小说保存完毕")
+
+
+def range_donwload(url: str, lower_bound: int, upper_bound: int) -> None:
     if lower_bound > upper_bound:
         lower_bound, upper_bound = upper_bound, lower_bound
     lower_bound -= 1  # 更加符合习惯用法
 
     crawler = Crawler()
+    log.info("🎉 DrissionPage初始化完毕")
+
     index = crawler.get_index(url)
-    info = RangeInfo(index.name, lower_bound, upper_bound, [])
-    atexit.register(save, info)
+    lower_name = index.chpts[lower_bound].name
+    upper_name = index.chpts[upper_bound].name
+    log.info(f"🎈 正在下载{index.name}，范围从 {lower_name} 到 {upper_name}")
 
+    chpts: list[str] = []
     with Progress() as progress:
-        download = progress.add_task("🚚 下载中", total=upper_bound - lower_bound)
-        for url in index.urls[lower_bound:upper_bound]:
-            chpt = crawler.get_chpt(url)
-            info.chpts.append(chpt)
-            progress.advance(download)
-            time.sleep(random.randint(5, 9) + random.random())
-
-
-def download_full_content(url: str) -> None:
-    crawler = Crawler()
-    index = crawler.get_index(url)
-    info = FullInfo(index.name, len(index.urls), [])
-    atexit.register(save, info)
-
-    with Progress() as progress:
-        download = progress.add_task("🚚 下载中", total=len(index.urls))
-        for url in index.urls:
-            chpt = crawler.get_chpt(url)
-            info.chpts.append(chpt)
-            progress.advance(download)
-            time.sleep(random.randint(5, 9) + random.random())
-
-
-@functools.singledispatch
-def save(info: Any) -> None:
-    raise RuntimeError("Unreachable!")
-
-
-@save.register
-def _(info: FullInfo) -> None:
-    if info.is_finished:
-        log.info("✨ 小说下载成功")
-    else:
-        log.warning("❗ 小说下载未完成")
-        log.info(f"📌 已下载至{info.progress}章")
-
-    with open(f"{info.name}-full.txt", "a", encoding="utf-8") as f:
-        f.write("\n".join(info.chpts))
-    log.info("✨ 小说保存成功")
-
-
-@save.register
-def _(info: RangeInfo) -> None:
-    if info.is_finished:
-        log.info("✨ 小说分章节下载成功")
-    else:
-        log.warning("❗ 小说下载未完成")
-        log.info(f"📌 已下载至{info.progress}章")
-
-    with open(
-        f"{info.name}-{info.lower_bound}-{info.progress}.txt", "w", encoding="utf-8"
-    ) as f:
-        f.write("\n".join(info.chpts))
-    log.info("✨ 小说分章节保存成功")
-
-
-def main() -> None:
-    parser = create_parser()
-    args = parser.parse_args()
-
-    if args.mode == "full":
-        log.info(f"🎉 正在从URL下载全文内容：{args.url}")
-        # 调用下载全文的函数
-        download_full_content(args.url)
-    elif args.mode == "range":
-        if args.upper_bound is None or args.lower_bound is None:
-            parser.error("在范围模式下，必须同时提供 --upper-bound 和 --lower-bound")
-        log.info(
-            f"🎉 正在从URL下载内容：{args.url}，范围从 {args.lower_bound} 到 {args.upper_bound}"
-        )
-        # 调用下载指定范围的函数
-        download_range_content(args.url, args.lower_bound, args.upper_bound)
-
-
-if __name__ == "__main__":
-    try:
-        main()
-    except Exception as e:
-        log.error(e)
+        download = progress.add_task("🛻 下载中")
+        try:
+            for info in index.chpts[lower_bound:upper_bound]:
+                chpt = crawler.get_chpt(info.url)
+                chpts.append(chpt)
+                progress.advance(download)
+        except Exception as e:
+            log.error(e)
+        finally:
+            content = "\n".join(chpts)
+            save(f"{index.name}-{lower_bound}-{upper_bound}", content)
+            log.info("✨ 小说保存完毕")
--- utils.py
+++ utils.py
@@ -1,5 +1,4 @@
 import logging
-
 from typing import NamedTuple
 
 from DrissionPage import ChromiumPage
@@ -14,30 +13,35 @@ logging.basicConfig(
 log = logging.getLogger("rich")
 
 
+class ChapterInfo(NamedTuple):
+    name: str
+    url: str
+
+
 class Index(NamedTuple):
     name: str
-    urls: list[str]
+    chpts: list[ChapterInfo]
 
 
 class Crawler:
     def __init__(self) -> None:
         self.page = ChromiumPage()
-        log.info("🎉 DrissionPage 初始化成功")
 
     def get_index(self, url: str) -> Index:
         self.page.get(url)
         name = self.page.ele("#bookName").text
         elems = self.page.s_eles(".chapter-name")
-        urls = []
+        urls: list[ChapterInfo] = []
         for elem in elems:
+            name = elem.text
             href = elem.attr("href")
             if href is not None:
-                urls.append(href)
-        return Index(name, urls)  # type: ignore
+                urls.append(ChapterInfo(name, href))
+        return Index(name, urls)
 
     def get_chpt(self, chpt: str) -> str:
         self.page.get(chpt)
-        content = []
+        content: list[str] = []
         title = self.page.ele(".:title").text
         content.append(title)
         for elem in self.page.eles(".content-text"):
